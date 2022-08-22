64d574fb96b772b692188ad69c5ff40e7f640832
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-08-22 14:37:59 +0200

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.html"),
+  entry: path.resolve(__dirname, "./src/app.js"),
   mode: "development",
   output: {
     filename: "[name].bundle.js",
@@ -37,6 +37,16 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
 };
