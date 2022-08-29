82d30c43c498657aeff7d76e3dafdfc3bf6da8c7
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-08-29 15:08:38 +0200

    added new plugings

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
       template: path.resolve(__dirname, "./src/pages/home/index.html"),
       filename: "index.html",
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/pages/services/index.html"),
+      filename: "services.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/pages/contact/index.html"),
+      filename: "contact.html",
+    }),
   ],
   plugins: [
     new CopyPlugin({
