158c3d5bbcedbee2338a3962729ceb26489f5dc8
Rube3n <rubenberthepro@gmail.com>
2022-09-02 21:31:45 +0200

    js menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,14 @@
 const menuMobile = document.getElementById("menu-mobile");
 const navMenu = document.getElementById("nav-links")
+const menuClose = document.getElementById("close")
 
 const Menu = () => {
   const showMenu = () => {
-    navMenu.classList.toggle('hidden')
+    navMenu.classList.toggle('hidden');
+    
   };
   menuMobile.addEventListener("click", showMenu);
+  menuClose.addEventListener("click", showMenu);
 };
 
 export default Menu
