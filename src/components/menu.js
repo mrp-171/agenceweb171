b5f78ec079c856d5c3828a68bf9a9158d142f769
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-09-09 13:39:36 +0200

    finalize project

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,4 @@
-// MENU HAMBURGER
-// const menuMobile = document.getElementById("menu-mobile");
-// const navMobile = document.getElementById("nav-mobile");
-// const root = document.getElementById("root");
 
-// const Menu = () => {
-//   const showMenu = () => {
-//     navMobile.classList.toggle(hidden);
-//   };
-
-// const closeMenu = () => {
-// navMobile.classList.contains("hidden") ? console.log("true") : console.log("false"); 
-// }
-
-//   menuMobile.addEventListener("click", showMenu);
-
-//   root.addEventListener("click", closeMenu);
-// };
-
-// export default Menu;
 const body = document.querySelector("body");
 const menuMobile = document.getElementById("menu-mobile");
 const navMenu = document.getElementById("nav-links");
